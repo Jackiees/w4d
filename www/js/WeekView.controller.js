4112b5a0fcffd71a27a0b7d8d21187e8869ecddc
unknown <KALLAW@LBNL031037.corp.capgemini.com>
2016-01-17 01:32:23 +0100

    basic meal editing added

--- www/js/WeekView.controller.js
+++ www/js/WeekView.controller.js
@@ -4,6 +4,51 @@ angular.module('WeekView.controller', [])
 
 $scope.goTo = function(stateName) {
     $state.go(stateName)
- }
+};
+
+$scope.WeekPlanArray = [
+	{day: "MON",
+	 date: "1",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "TUE",
+	 date: "2",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "WED",
+	 date: "3",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "THU",
+	 date: "4",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "FRI",
+	 date: "5",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "SAT",
+	 date: "6",
+	 meal: " ",
+	 edit: false
+	},
+	{day: "SUN",
+	 date: "7",
+	 meal: " ",
+	 edit: false
+	},
+];
+	
+//console.log($scope.WeekPlanArray);
+
+$scope.toggleEdit = function () {
+	this.i.edit = !this.i.edit;
+//	console.log(this.i.edit);
+};
 	 
 });
